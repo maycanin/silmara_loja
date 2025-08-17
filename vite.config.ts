e4e0808ccbf6409cc4db95ccb7b5e119a7971574
mayson.inacio <maysoninacio@gmail.com>
2025-08-17 20:37:34 -0300

    ajuste vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,20 @@ import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    react(),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  plugins: [
+    react(), 
     cloudflare()
   ],
-  server: {
-    allowedHosts: true,
-  },
-  build: {
-    chunkSizeWarningLimit: 5000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./functions/src"),
-    },
-  },
+  server: {
+    allowedHosts: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 5000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./functions/src"),
+    },
+  },
 });
